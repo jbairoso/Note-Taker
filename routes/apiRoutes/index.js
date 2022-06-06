d172c6fb1c5c1ebae495b1fba9ecadf64d097db4
jbairoso <jbairoso@gmail.com>
2022-06-05 20:09:09 -0700

    htmlroute set up

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,4 +3,8 @@ const fs = require('fs');
 const router = require("express").Router();
 const path = require("path");
 
+const getText = () => {
+    const notes = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
+    return JSON.parse (notes);
+}
 module.exports = router;
