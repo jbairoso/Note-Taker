c66baa2df3031faa80cd13b0b49ddb2c4fa7dd9b
jbairoso <jbairoso@gmail.com>
2022-06-05 21:50:53 -0700

    debug

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require ("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 //parse data
 app.use (express.urlencoded({ extended: true }));
 app.use (express.static('public'));
@@ -14,8 +13,6 @@ app.use (express.json());
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-
-
-app.listen( PORT, () =>{
+app.listen(PORT, () =>{
     console.log (`API server now on port ${PORT}!`);
 });
