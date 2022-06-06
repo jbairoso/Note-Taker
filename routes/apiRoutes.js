931b23e161538fd1a09c5adebb9e1db96ee9274f
jbairoso <jbairoso@gmail.com>
2022-06-05 22:41:29 -0700

    solve for not saving

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,25 @@
 //file system
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 //routing
 module.exports = function (app) {
-
   // GET api request
-  app.get('/api/notes', (request, response) => {
-    let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+  app.get("/api/notes", (request, response) => {
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     response.json(data);
   });
 
   //POST api request
-  app.post('/api/notes', (request, response) => {
-    const newNotes = req.body;
-    newNotes.id = uuidv4;
+  app.post("/api/notes", (request, response) => {
+    const newNote = request.body;
+    newNote.id = (uuidv4);
     //reads
-    let data = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"));
-    data.push(newNotes);
+    let data = JSON.parse(
+      fs.readFileSync("./db/db.json", "utf8"));
+    data.push(newNote);
     //writes
-    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(data));
-    res.json(data);
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    response.json(data);
   });
 };
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-//require express 
-const express = require ("express");
-//using port
-const PORT = process.env.PORT || 3001;
+//require express
+const express = require("express");
 const app = express();
 
+//using port
+const PORT = process.env.PORT || 3001;
+
 //parse data
-app.use (express.urlencoded({ extended: true }));
-app.use (express.static('public'));
-app.use (express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
 
 //routes
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
-    console.log (`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
