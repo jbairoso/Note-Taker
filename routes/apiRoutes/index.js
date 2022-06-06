90466dc092abc2ddc2139410be0622ef9a8ff27a
jbairoso <jbairoso@gmail.com>
2022-06-05 20:45:25 -0700

    post api

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,23 @@
 //file system
-const fs = require('fs');
+const fs = require("fs");
+const { request } = require("http");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 //routing
-module.exports = function(app){
+module.exports = function (app) {
+  // GET api request
+  app.get("/api/notes", (request, response) => {
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    response.json(data);
+  });
 
-    // get request 
-    app.get("/api/notes", (request, response) => {
-        let data = JSON.parse (fs.readFileSync("./db/db.json", "utf8"));
-        response.json(data);
-    });
-
-    //
-}
-
+  //POST api request
+  app.post("/api/notes", (request, response) => {
+    const newNotes = req.body;
+    newNotes.id = uuidv4;
+    let data = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8"));
+    data.push(newNotes);
+  });
+};
