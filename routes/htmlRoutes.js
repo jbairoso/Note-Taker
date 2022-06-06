3395ccf85443eb6b27bc484e71baf762321f3903
jbairoso <jbairoso@gmail.com>
2022-06-05 21:40:44 -0700

    consolidate routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+//routing
+module.exports = function(app) {
+    //GET/notes return notes.html
+  app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/notes.html'))
+  });
+
+  //GET * will return index.html
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..public/index.html'))
+  });
+}
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 //require express 
 const express = require ("express");
-
-
+//using port
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+//parse data
+app.use (express.urlencoded({ extended: true }));
+app.use (express.static('public'));
+app.use (express.json());
 
 //routes
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-app.use (express.urlencoded({ extended: true }));
-app.use (express.static('public'));
-app.use (express.json());
-app.use ('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 
 app.listen( PORT, () =>{
-    console.log ('API server now on port ${PORT}!');
+    console.log (`API server now on port ${PORT}!`);
 });
